a5e8f8ba4c68e78ecacf3aca91d9cb322c1b33ac
sungjk <ajax0615@gmail.com>
2022-01-23 16:15:48 +0900

    Fix build failure on Apple M1

--- install-mac.sh
+++ install-mac.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install ruby
-sudo gem install github-pages
-sudo gem install pygments.rb
+brew install rbenv ruby-build
+echo 'if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi' >> ~/.zshrc
+source ~/.zshrc
+rbenv install 3.0.0
+rbenv global 3.0.0
+ruby -v
+rbenv rehash
+gem install github-pages
+gem install pygments.rb
+gem install webrick
 echo "You can run the server by using 'jekyll serve'."
